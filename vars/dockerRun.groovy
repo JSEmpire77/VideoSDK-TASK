98df4374e39ac1f2c02f378aac2f93d301ccda10
JSEmpire77 <114047074+JSEmpire77@users.noreply.github.com>
2025-09-19 16:29:08 +0530

    Update dockerRun.groovy

--- vars/dockerRun.groovy
+++ vars/dockerRun.groovy
@@ -1,17 +1,8 @@
 def call(String DockerHubUser, String ImageTag ,String ProjectName){
-
-    def oldTag = sh(
-        script: "docker ps --filter 'name=ws' --format '{{.Image}}' | awk -F ':' '{print \$2}'",
-        returnStdout: true
-    ).trim()
-
-  if (oldTag && oldTag != ImageTag) {
-        echo "Old container with tag ${oldTag} found. Removing..."
-        sh "docker rm -f ws} || true"
-    }
-
+  sh "docker rm -f ws || true"
   echo "Deploying container with tag ${ImageTag}"
   sh "docker run -d -p 9090:8080 --name=ws ${DockerHubUser}/${ProjectName}:${ImageTag}"
 }
+
 
 
