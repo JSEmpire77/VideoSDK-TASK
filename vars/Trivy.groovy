b294dc5be9bf481e8dac5675eea28a3e259029d8
JSEmpire77 <114047074+JSEmpire77@users.noreply.github.com>
2025-09-24 13:53:41 +0530

    Update Trivy.groovy

--- vars/Trivy.groovy
+++ vars/Trivy.groovy
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 def call(){
   sh "trivy fs --format table -o trivy-fs-report.html ."
 }
-=======
-def call(){
-  sh "trivy fs --format table -o trivy-fs-report.html ."
-}
->>>>>>> 676d821bc76ef755d72f4fbd783afbcd4fd1633f
