d11f001062952efce0bb23d5779b4860ac0728a1
JSEmpire77 <114047074+JSEmpire77@users.noreply.github.com>
2025-09-24 14:36:02 +0530

    Update dockerRun.groovy

--- vars/dockerRun.groovy
+++ vars/dockerRun.groovy
@@ -11,7 +11,7 @@ def call(String DockerHubUser, String ImageTag ,String ProjectName){
     }
   sh "docker rm -f ws} || true"
   echo "Deploying container with tag ${ImageTag}"
-  sh "docker run -d -p 8080:8080 -name=ws ${DockerHubUser}/${ProjectName}:${ImageTag}"
+  sh "docker run -d -p 8080:8080 --name=ws ${DockerHubUser}/${ProjectName}:${ImageTag}"
 }
 // The project focused on deploying a WebSocket server and applying load testing to evaluate system stability under real-time communication with multiple concurrent users. The server was developed and containerized with custom metrics exposed on the /metrics endpoint, enabling monitoring and observability. To validate performance, K6 load testing was integrated to simulate high traffic and measure system responsiveness. A CI/CD pipeline was configured in Jenkins to fully automate the workflow, including code checkout from GitHub, SonarQube analysis for code quality gates, OWASP Dependency Check for library vulnerabilities, Trivy scanning for container image vulnerabilities, Docker image build and push to a private Docker Hub repository, and finally automatic container deployment on the target server.
 
