980f9cc4b4b2a780b6ba32e7eacd9be6f6999698
JSEmpire77 <114047074+JSEmpire77@users.noreply.github.com>
2025-09-24 12:30:07 +0530

    Update CleanWs.groovy

--- vars/CleanWs.groovy
+++ vars/CleanWs.groovy
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 def call() {
     echo "Cleaning up workspace..."
     cleanWs()
 }
-=======
-def call() {
-    echo "Cleaning up workspace..."
-    cleanWs()
-}
->>>>>>> 676d821bc76ef755d72f4fbd783afbcd4fd1633f
+
