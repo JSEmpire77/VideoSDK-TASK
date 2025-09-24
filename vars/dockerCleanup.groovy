7e09d49b41ba11830957bf12345219b243c11dc1
JSEmpire77 <114047074+JSEmpire77@users.noreply.github.com>
2025-09-24 13:54:31 +0530

    Update dockerCleanup.groovy

--- vars/dockerCleanup.groovy
+++ vars/dockerCleanup.groovy
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 def call(String DockerHubUser , String ImageTag , String Project){
   sh "docker rmi ${DockerHubUser}/${Project}:${ImageTag}"
 }
-=======
-def call(String DockerHubUser , String ImageTag , String Project){
-  sh "docker rmi -f ${DockerHubUser}/${Project}:${ImageTag}"
-}
 
->>>>>>> 676d821bc76ef755d72f4fbd783afbcd4fd1633f
