bb0e006a536821466c9ca38776bf3d9b484f92a4
JSEmpire77 <114047074+JSEmpire77@users.noreply.github.com>
2025-09-24 13:55:40 +0530

    Update sonarQuailityGate.groovy

--- vars/sonarQuailityGate.groovy
+++ vars/sonarQuailityGate.groovy
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 def call(){
   timeout(time: 1, unit: "MINUTES"){       // wait for 1 minute ater that move to next stage.
       waitForQualityGate abortPipeline: false
   }
 }
-=======
-def call(){
-  timeout(time: 1, unit: "MINUTES"){       // wait for 1 minute ater that move to next stage.
-      waitForQualityGate abortPipeline: false
-  }
-}
->>>>>>> 676d821bc76ef755d72f4fbd783afbcd4fd1633f
+
