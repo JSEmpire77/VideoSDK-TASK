327a8717373a6265f647ef020289235bb49aecd4
JSEmpire77 <114047074+JSEmpire77@users.noreply.github.com>
2025-09-24 13:54:15 +0530

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 def call(String DockerHubUser ,  String ImageTag , String ProjectName){
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ./server/Dockerfile"
 }
-=======
-def call(String DockerHubUser ,  String ImageTag , String ProjectName){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} -f ./server/Dockerfile ./server"
-}
 
-
->>>>>>> 676d821bc76ef755d72f4fbd783afbcd4fd1633f
