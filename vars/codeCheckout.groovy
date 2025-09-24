a6e289fca95f1a7ce66d66b1f5a81f2c520387c8
JSEmpire77 <114047074+JSEmpire77@users.noreply.github.com>
2025-09-24 13:53:57 +0530

    Update codeCheckout.groovy

--- vars/codeCheckout.groovy
+++ vars/codeCheckout.groovy
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 def call(String url, String branch){
   git url: "${url}", branch: "${branch}"
 }
-=======
-def call(String url, String branch){
-  git url: "${url}", branch: "${branch}"
-}
->>>>>>> 676d821bc76ef755d72f4fbd783afbcd4fd1633f
