fa1398cc51dfdbe35d73fd81e8486cc0bbc6778d
JSEmpire77 <114047074+JSEmpire77@users.noreply.github.com>
2025-09-24 13:55:58 +0530

    Update sonarQubeScan.groovy

--- vars/sonarQubeScan.groovy
+++ vars/sonarQubeScan.groovy
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 def call(String SonarQubeAPI, String Projectname, String ProjectKey){
   withSonarQubeEnv("${SonarQubeAPI}"){
       sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
   }
 }
-=======
-def call(String SonarQubeAPI, String Projectname, String ProjectKey){
-  withSonarQubeEnv("${SonarQubeAPI}"){
-      sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${Projectname} -Dsonar.projectKey=${ProjectKey} -X"
-  }
-}
->>>>>>> 676d821bc76ef755d72f4fbd783afbcd4fd1633f
